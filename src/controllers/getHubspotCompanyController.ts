28b57e85d6aff839d028ed6dc3b9c35546383423
Nomekuma <t.guness35@gmail.com>
2024-02-29 11:26:52 +0400

    Feat: Get Hubspot Company Data

---
+++ src/controllers/getHubspotCompanyController.ts
@@ -0,0 +1,29 @@
+import { hubspotClient } from "../utils/hubspotConfig";
+import { Request, Response } from "express";
+
+const getHubspotCompany = async (req: Request, res: Response) => {
+  const limit = undefined;
+  const after = undefined;
+  const properties = undefined;
+  const propertiesWithHistory = undefined;
+  const associations = undefined;
+  const archived = false;
+
+  try {
+    const apiResponse = await hubspotClient.crm.companies.basicApi.getPage(
+      limit,
+      after,
+      properties,
+      propertiesWithHistory,
+      associations,
+      archived
+    );
+    res.status(200).json(apiResponse);
+  } catch (e: any) {
+    e.message === "HTTP request failed"
+      ? res.status(500).json(e.response)
+      : res.status(500).json(e);
+  }
+};
+
+export { getHubspotCompany };
